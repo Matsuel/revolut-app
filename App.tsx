547a2bd614a59036319fc34e17b0387375f536e7
Matsuel <matsuel0308@gmail.com>
2024-04-11 18:27:43 +0200

    add statut bar style

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import Welcome from './src/Screens/Welcome/Welcome';
 import SplashScreen from './src/Screens/SplashScreen/SplashScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="light" />
       <Stack.Navigator>
         <Stack.Screen
           name="SplashScreen"
