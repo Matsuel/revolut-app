edca64b13bba823522657be12fbc94e2fa93a0c9
Matsuel <matsuel0308@gmail.com>
2024-04-17 11:28:59 +0200

    add title and subtitle to notifs

--- src/Screens/AcceptNotifications/AcceptNotifications.style.tsx
+++ src/Screens/AcceptNotifications/AcceptNotifications.style.tsx
@@ -10,4 +10,27 @@ export const styles = StyleSheet.create({
         paddingLeft: "5%",
         paddingRight: "5%",
     },
+    top: {
+        width: "100%",
+        height: "auto",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        gap: 10,
+        marginTop: "20%",
+    },
+    title: {
+        width: "90%",
+        color: "#000",
+        fontSize: 32,
+        fontWeight: "700",
+        marginTop: "5%",
+    },
+    subtitle: {
+        width: "90%",
+        color: "#000",
+        fontSize: 16.5,
+        fontWeight: "700",
+        marginTop: "7%",
+    },
 });
--- src/Screens/AcceptNotifications/AcceptNotifications.tsx
+++ src/Screens/AcceptNotifications/AcceptNotifications.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { styles } from './AcceptNotifications.style'
-import { Text, View } from 'react-native'
+import { Text, View, Image, TouchableOpacity } from 'react-native'
+import LeftArrow from '../../assets/LeftArrow'
+import Logo from '../../assets/Logo'
 
-const AcceptNotifications = () => {
+const AcceptNotifications = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
-      <Text>
-        AcceptNotifications
+      <View style={styles.top}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <LeftArrow color="#000" />
+        </TouchableOpacity>
+        <Logo />
+      </View>
+      <Text style={styles.title}>
+        Don't miss a beat
       </Text>
+      <Text style={styles.subtitle}>
+        Get notified about spending, security, whealth, market movements, discounts and deals, so you're always in the know
+      </Text>
+      <Image source={require('../../assets/Notifs.png')} />
     </View>
   )
 }
